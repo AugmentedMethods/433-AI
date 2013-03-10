0b5b1f5608728d6c379447ac8ee8953e03c9f665
AugmentedMethods <Robertshaw.Sean@gmail.com>
2013-03-09 23:36:55 -0700

    First attempt

---
+++ Person/src/Person.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sean
+ * Date: 3/9/13
+ * Time: 10:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Person {
+
+    public static void main (String args[])
+    {
+        System.out.println("This is a test");
+
+    }
+
+}
