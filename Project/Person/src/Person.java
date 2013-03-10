7fe79edbefb2178c2b36673c24866cd0abd3682b
AugmentedMethods <Robertshaw.Sean@gmail.com>
2013-03-10 15:04:17 -0600

    Beginning

---
+++ Project/Person/src/Person.java
@@ -0,0 +1,27 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sean
+ * Date: 3/10/13
+ * Time: 2:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Person {
+
+    //Hard constraints
+    private boolean assignedRoom;
+    private boolean assignedOneRoom;  //this might be redundant
+    private int numInRoom;
+    private boolean shareRoom;
+
+    //soft constraints
+    private int officeSize; //1 = small, 2 = medium, 3= large
+    private boolean secretaryClose;
+    private boolean managerCloseGH;
+
+
+    private boolean smoker;
+
+
+
+
+}
