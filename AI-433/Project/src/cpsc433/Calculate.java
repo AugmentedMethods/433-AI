2344127adca42d6f810906d668f994fe58505ef9
Alternalia <nothing_to_it@hotmail.com>
2013-04-06 12:51:32 -0600

    Soft Constraints mostly added
    Missing 2, 4, 7, 8.
    Am I mistaken in thinking that we need to have all of these to get an accurate goodness value?

--- AI-433/Project/src/cpsc433/Calculate.java
+++ AI-433/Project/src/cpsc433/Calculate.java
@@ -6,7 +6,7 @@ package cpsc433;
 public class Calculate {
 
     /**
-     * The basic entry function to preform a calculation
+     * The basic entry function to perform a calculation
      * @param node
      * @return true if a valid node, false otherwise.
      */
@@ -28,14 +28,14 @@ public class Calculate {
 
     }
 
-    private void groupHeadLargeOffice(Node node)
+    private void groupHeadLargeOffice(Node node) // SC1
     {
         if(node.getRoom().isLarge())
             return;
         node.setGoodnessValue(node.getGoodnessValue() -40);
     }
 
-    private void closeToSecretary(Node node)
+    private void closeToSecretary(Node node) // SC3
     {
         for( Rooms r : node.getRoom().getClose())
         {
@@ -53,7 +53,7 @@ public class Calculate {
         node.setGoodnessValue(node.getGoodnessValue() -30);
     }
 
-    private void managerCloseGroupHead(Node node)
+    private void managerCloseGroupHead(Node node) //SC6
     {
         for( Rooms r : node.getRoom().getClose())
         {
@@ -71,7 +71,7 @@ public class Calculate {
         node.setGoodnessValue(node.getGoodnessValue() -20);
     }
 
-    private void smoker(Node node)
+    private void smoker(Node node) // SC11
     {
         //xnor
         boolean temp = !((node.getRoom().getPersonOne().isSmoker()) ^  (node.getRoom().getPersonTwo().isSmoker()) );
@@ -80,5 +80,128 @@ public class Calculate {
         node.setGoodnessValue(node.getGoodnessValue() -50);
 
     }
-}
+    
+    private void managerCloseSecretary(Node node) // SC5
+    {
+        for( Rooms r : node.getRoom().getClose())
+        {
+            if(r.getPersonOne() != null)
+            {
+                if(r.getPersonOne().isSecretary())
+                    return;
+            }
+            if(r.getPersonTwo() != null)
+            {
+                if(r.getPersonTwo().isSecretary())
+                    return;
+            }
+        }
+        node.setGoodnessValue(node.getGoodnessValue() -20);
+    }
+    
+    private void headsLargeProjectsCloseSecretary(Node node) // SC9
+    {
+        if(node.getRoom().getPersonOne().isHeadsProject() || node.getRoom().getPersonTwo().isHeadsProject())
+        {
+            for(Rooms r : node.getRoom().getClose())
+            {
+                if(r.getPersonOne() != null)
+                {
+                    if(r.getPersonOne().isSecretary())
+                    {
+                        if(node.getRoom().getPersonOne().getProject() == r.getPersonOne().getProject() || 
+                        node.getRoom.getPersonTwo().getProject() == r.getPersonOne().getProject())
+                            return;
+                    }
+                }
+                if(r.getPersonTwo() != null)
+                {
+                    if(r.getPersonTwo().isSecretary())
+                    {
+                        if(node.getRoom().getPersonOne().getProject() == r.getPersonOne().getProject() || 
+                        node.getRoom().getPersonTwo().getProject() == r.getPersonTwo().getProject())
+                            return;
+                    }
+                }
+            }
+        }
+        node.setGoodnessValue(node.getGoodnessValue() -10);
+    }
+    
+    private void headsLargeProjectsCloseHeadGroup(Node node) // SC10
+    {
+        if(node.getRoom().getPersonOne().isHeadsProject() || node.getRoom().getPersonTwo().isHeadsProject())
+        {
+            for(Rooms r : node.getRoom().getClose())
+            {
+                if(r.getPersonOne() != null)
+                {
+                    if(r.getPersonOne().isHeadsGroup())
+                    {
+                        if(node.getRoom().getPersonOne().getGroup() == r.getPersonOne().getGroup() || 
+                        node.getRoom.getPersonTwo().getGroup() == r.getPersonOne().getGroup())
+                            return;
+                    }
+                }
+                if(r.getPersonTwo() != null)
+                {
+                    if(r.getPersonTwo().isHeadsGroup())
+                    {
+                        if(node.getRoom().getPersonOne().getGroup() == r.getPersonOne().getGroup() || 
+                        node.getRoom().getPersonTwo().getGroup() == r.getPersonTwo().getGroup())
+                            return;
+                    }
+                }
+            }
+        }
+        node.setGoodnessValue(node.getGoodnessValue() -10);
+    }
+    
+    private void sameProjectNotShareRoom(Node node) // SC12
+    {
+        if(node.getRoom().getPersonOne() == null)
+            return;
+        if(node.getRoom().getPersonTwo() == null)
+            return;
+        if(!(node.getRoom().getPersonOne().getProject() == node.getRoom().getPersonTwo().getProject()))
+            return;
+        node.setGoodnessValue(node.getGoodnessValue() -7);
+    }
+    
+    private void SC13(Node node) //SC13 - Not sure what's going on here...
+    {
+    }
+    
+    private void wantOwnRoom(Node node) //SC14
+    {
+        if(node.getRoom().getPersonOne() == null)
+            return;
+        else if(node.getRoom().getPersonTwo() == null)
+            return;
+        else
+            node.setGoodnessValue(node.getGoodnessValue() -4);
+    }
+    
+    private void sharingShouldWorkTogether(Node node) // SC15
+    {
+        if(node.getRoom().getPersonOne() == null)
+            return;
+        if(node.getRoom().getPersonTwo() == null)
+            return;
+        if(!(node.getRoom().getPersonOne().getWorksWith().contains(node.getRoom().getPersonTwo()))) // Not sure about logic
+            return;
+        node.setGoodnessValue(node.getGoodnessValue() -3);
+    }
 
+    private void dontShareSmallRoom(Node node) // SC16
+    {
+        if(node.getRoom().isSmall)
+        {
+            if(node.getRoom().getPersonOne() == null)
+                return;
+            if(node.getRoom().getPersonTwo() == null)
+                return;
+        }
+        node.setGoodnessValue(node.getGoodnessValue() -25);
+    }
+}
